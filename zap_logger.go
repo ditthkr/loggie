45ffbd1b6bc88030018470cfb5bbea086dddfc00
ditthkr <dev@ditthkr.com>
2025-05-04 18:56:45 +0700

    fix some one

--- zap_logger.go
+++ zap_logger.go
@@ -6,25 +6,25 @@ type ZapLogger struct {
 	L *zap.Logger
 }
 
-func (z *ZapLogger) Info(msg string, fields ...any) {
-	z.L.Info(msg, toZapFields(fields...)...)
+func (r *ZapLogger) Info(msg string, fields ...any) {
+	r.L.Info(msg, toZapFields(fields...)...)
 }
 
-func (z *ZapLogger) Error(msg string, fields ...any) {
-	z.L.Error(msg, toZapFields(fields...)...)
+func (r *ZapLogger) Error(msg string, fields ...any) {
+	r.L.Error(msg, toZapFields(fields...)...)
 }
 
-func (z *ZapLogger) With(fields ...any) Logger {
-	return &ZapLogger{L: z.L.With(toZapFields(fields...)...)}
+func (r *ZapLogger) With(fields ...any) Logger {
+	return &ZapLogger{L: r.L.With(toZapFields(fields...)...)}
 }
 
 func toZapFields(fields ...any) []zap.Field {
-	var zfields []zap.Field
+	var out []zap.Field
 	for i := 0; i < len(fields); i += 2 {
 		if i+1 < len(fields) {
 			key, _ := fields[i].(string)
-			zfields = append(zfields, zap.Any(key, fields[i+1]))
+			out = append(out, zap.Any(key, fields[i+1]))
 		}
 	}
-	return zfields
+	return out
 }
